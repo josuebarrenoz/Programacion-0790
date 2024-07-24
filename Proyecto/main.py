a601fbf617eaa067d9a5cbbe669a65328a0f07da
josue <maharba.josue@gmail.com>
2024-07-24 18:05:57 -0400

    Now exists cifrados.json

--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,6 +1,7 @@
 import msg, repet
 import modulo.validation as val
 import modulo.crypto as cry
+import json
 
 
 def listortuple(palabra):
@@ -24,9 +25,6 @@ def submenu_val1(opcion):
         pass
 
 
-
-
-
 def menu_val():
     while True:
         opcion=input("¿Que desea validar?\n\n1.Entero\n2.Flotante\n3.Complejo\n4.Lista\n\nEscribe la opcion: ")
@@ -40,17 +38,63 @@ def menu_val():
             print("\nEscribe una opcion valida")
 
 def json_cry(palabra):
-    pass
+    try:
+        with open ("cifrados.json","r") as file:
+            data = json.load(file)
+        file.close()
+    except FileNotFoundError:
+        print("\nNo existe ningun archivo Json con clave. ")
+        with open ("cifrados.json","w") as file:
+            data = {}
+        file.close()
+
+def primos_simp_cry(palabra, *abc):
+        while True:
+            quest=input("\nDescifrar o cifrar? \n\nOpciones: \n\n1. Cifrar \n2.Descifrar\n\nOpcion: ")
+            if quest=="1":
+                print(f"\nLa palabra cifrada es {cry.primos(palabra,1,*abc)}")
+                break
+            if quest =="2":
+                print(f"\nLa palabra descifrada es {cry.primos(palabra,2,*abc)}")
+                break
+            print("\nEscribe una opcion valida")
+
+
+def cesar_simp_cry(palabra, *abc):
+    while True:
+        try:
+            clave = int(input("\nEscribe la clave de cifrado: "))
+            break
+        except ValueError:
+            print("\nEscribe un Numero Entero")
+    while True:
+        quest=input("\nDescifrar o cifrar? \n\nOpciones: \n\n1. Cifrar \n2.Descifrar\n\nOpcion: ")
+        if quest=="1":
+            print(f"\nLa palabra cifrada es {cry.cesar(palabra,1,clave,*abc)}")
+            break
+        if quest =="2":
+            print(f"\nLa palabra descifrada es {cry.cesar(palabra,2,clave,*abc)}")
+            break
+        print("\nEscribe una opcion valida")
+
 
 def simp_cry(palabra, *abc):
     while True:
-        opcion=input("¿Quieres encriptar con Cesar ó Primos?\n\n1. Cesar\n2. Primos")
+        opcion=input("¿Quieres encriptar con Cesar ó Primos?\n\n1. Cesar\n2. Primos\n\nEscribe una opcion: ")
+        if opcion=="1":
+            cesar_simp_cry(palabra,*abc)
+            break
+        if opcion=="2":
+            primos_simp_cry(palabra,*abc)
+            break
+        print("\nEscribe una opcion valida")
 
 
 def menu_cry():
     palabra = input("\nEscribe una palabra a cifrar/descifrar: ")
+    palabra=palabra.lower()
     while True:
-        opcion=input("¿Quieres encriptar bajo un archivo Json? (s/n)")
+        opcion=input("\n¿Quieres encriptar bajo un archivo Json? (s/n): ")
         if opcion.lower() == "y":
             json_cry(palabra)
             break
@@ -66,9 +110,11 @@ def menu_grande():
         opcion = input("¿Que programa utilizaras?\n\n1.Validacion\n2.Crypto\n\nEscribe la opcion: ")
         if opcion=="1":
             menu_val()
+            repet.ition(menu_val,"¿Otra Validacion? (y/n): ", "")
             break
         elif opcion=="2":
             menu_cry()
+            repet.ition(menu_cry,"¿Otra frase a encriptar? (y/n): ", "")
             break
         else:
             print("\nEscribe una opcion valida")
--- Proyecto/repet.py
+++ Proyecto/repet.py
@@ -1,10 +1,10 @@
-def ition(func):
+def ition(func,frase1="Repetimos el programa? (y/n): ",frase2="Gracias por usar el programa"):
     while True:
-        rep = input("Repetimos el programa? (y/n): ")
+        rep = input(frase1)
         if rep.lower() == "y":
             func()
         elif rep.lower()=="n":
-            print("Gracias por usar el programa")
+            print(frase2)
             break
         else:
             print("Escriba un valor valido")
